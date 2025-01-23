98c65390855dcfffc38344cecf92c375e1d544a4
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-23 01:53:59 +0100

    feat: :white_check_mark: adding login, register and protected rules for band and concert

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConcertController;
 use App\Http\Controllers\BandController;
+use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/concert', ConcertController::class);
-Route::apiResource('/band', BandController::class);
+//login and register routes
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+// Rutas protegidas: necesario usar token
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/concert', ConcertController::class);
+    Route::apiResource('/band', BandController::class);
+});
